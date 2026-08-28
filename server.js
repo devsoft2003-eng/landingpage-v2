e319f4700f037aa03fba32b8ea6ce8b44923207b
devsoft2003 <devsoft2003@gmail.com>
2026-08-28 12:49:59 +0530

    Added Server.js File

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const next = require("next");
+
+const port = Number(process.env.PORT) || 3000;
+
+const app = next({
+  dev: false,
+});
+
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  http
+    .createServer((req, res) => {
+      handle(req, res);
+    })
+    .listen(port, "0.0.0.0", () => {
+      console.log(`DevSoft running on port ${port}`);
+    });
+});
